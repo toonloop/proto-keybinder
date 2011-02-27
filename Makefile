bd549647e3a6b211e53b7b04435b555be25df953
Alexandre Quessy <alexandre@quessy.net>
2011-02-27 18:36:00 -0500

    initial import

---
+++ Makefile
@@ -0,0 +1,17 @@
+DEPS=clutter-1.0
+CFLAGS=`pkg-config --cflags $(DEPS)`
+LIBS=`pkg-config --libs $(DEPS)`
+
+all: run
+
+main.o: main.cpp
+	g++ -Wall -g $(CFLAGS) main.cpp -c -o main.o
+
+run: main.o
+	g++ -Wall -g $(LIBS) main.o -o run
+
+.PHONY: clean
+
+clean:
+	rm -f run *.o
+
---
+++ main.cpp
@@ -0,0 +1,57 @@
+/**
+ * Prototype for key stuff
+ */
+#include <clutter/clutter.h>
+
+static const gint WIN_W = 640;
+static const gint WIN_H = 480;
+
+typedef struct app_
+{
+    ClutterActor *rectangle;
+} App;
+
+static void key_event_cb(ClutterActor *actor, ClutterKeyEvent *event, gpointer data)
+{
+    App *self = static_cast<App *>(data);
+    (void) self;
+    switch (event->keyval)
+    {
+        case CLUTTER_Escape:
+            clutter_main_quit();
+            break;
+        case CLUTTER_space:
+            break;
+        default:
+            break;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    clutter_init(&argc, &argv);
+
+    ClutterActor *stage = NULL;
+    ClutterColor black = { 0x00, 0x00, 0x00, 0xff };
+    ClutterColor red = { 0xff, 0x00, 0x00, 0xff };
+    stage = clutter_stage_get_default();
+    clutter_stage_set_title(CLUTTER_STAGE(stage), "Key binder proto");
+    clutter_stage_set_color(CLUTTER_STAGE(stage), &black);
+    clutter_actor_set_size(stage, WIN_W, WIN_H);
+
+
+    App *self = g_new0(App, 1);
+
+    self->rectangle = clutter_rectangle_new_with_color(&red);
+    clutter_actor_set_size(self->rectangle, 200, 200);
+    clutter_container_add_actor(CLUTTER_CONTAINER(stage), self->rectangle);
+    
+    g_signal_connect(stage, "key-press-event", G_CALLBACK(key_event_cb), self);
+    clutter_actor_show(stage);
+    clutter_main();
+
+    g_free(self);
+
+    return 0;
+}
+
